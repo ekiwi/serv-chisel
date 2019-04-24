587657e37b41afc14fdd10b76c424aeca188a9b2
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-04-23 19:02:12 -0700

    add Chisel skeleton

---
+++ src/main.scala
@@ -0,0 +1,11 @@
+// Copyright 2019 The Regents of the University of California
+// Copyright 2019 Olof Kindgren
+// released under BSD 2-Clause License
+// author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+// based on Olof Kindgren's implementation in Verilog
+
+object main {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
