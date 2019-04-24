d67ee1edb7e4f4751594d13e66885008b5e73df2
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-04-23 19:08:24 -0700

    start on alu

---
+++ src/alu.scala
@@ -0,0 +1,15 @@
+// Copyright 2019 The Regents of the University of California
+// Copyright 2019 Olof Kindgren
+// released under BSD 2-Clause License
+// author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+// based on Olof Kindgren's implementation in Verilog
+
+
+import chisel3._
+import chisel3.util._
+
+class shift_reg(val LEN : Int = 0, val INIT : Int = 0) extends Module {
+  val io = IO(new Bundle {
+
+  })
+}
