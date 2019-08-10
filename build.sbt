81c1536353bb91bbe39c8e6f16216c14fcbc5492
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-08-09 17:38:33 -0700

    add AdapterModule in order to match the IO of the original verilog

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ scalaVersion := "2.12.7"
 
 scalacOptions := Seq("-deprecation", "-unchecked", "-Xsource:2.11")
 
-libraryDependencies += "edu.berkeley.cs" %% "chisel3" % "3.1.+"
+libraryDependencies += "edu.berkeley.cs" %% "chisel3" % "3.2-SNAPSHOT"
 libraryDependencies += "edu.berkeley.cs" %% "firrtl" % "1.1.+"
 libraryDependencies += "edu.berkeley.cs" %% "chisel-iotesters" % "1.2.+"
 libraryDependencies += "edu.berkeley.cs" %% "treadle" % "1.0.+"
--- src/main.scala
+++ src/main.scala
@@ -7,5 +7,27 @@
 import chisel3._
 
 object main extends App {
-  Driver.execute(args, () => new serv_alu())
+
+  val m: Map[String, String] = Map(
+    "en"        -> "i_en",
+    "rs1"       -> "i_rs1",
+    "op_b"      -> "i_op_b",
+    "buf"       -> "i_buf",
+    "init"      -> "i_init",
+    "cnt_done"  -> "i_cnt_done",
+    "sub"       -> "i_sub",
+    "bool_op"   -> "i_bool_op",
+    "cmp.sel"   -> "i_cmp_sel",
+    "cmp.neg"   -> "i_cmp_neg",
+    "cmp.uns"   -> "i_cmp_uns",
+    "cmp.out"   -> "o_cmp",
+    "shamt_en"  -> "i_shamt_en",
+    "sh.right"  -> "i_sh_right",
+    "sh.signed" -> "i_sh_signed",
+    "sh.done"   -> "o_sh_done",
+    "rd.sel"    -> "i_rd_sel",
+    "rd.out"    -> "o_rd"
+  )
+
+  Driver.execute(args, Adapter(() => new serv_alu(), "clk", "i_rst", m))
 }
