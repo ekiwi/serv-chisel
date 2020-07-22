d09b93e7c9026d5c13bba1ae7dc721ee64830c0a
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-07-21 18:02:08 -0700

    finish RegisterFileInterface

--- src/serv/RegisterFileInterface.scala
+++ src/serv/RegisterFileInterface.scala
@@ -7,31 +7,93 @@
 package serv
 
 import chisel3._
+import chisel3.util._
 
-// This mostly muxes register file and CSR accesses to use the same RAM
+// This mostly muxes register file, CSR and trap accesses to use the same RAM
 class RegisterFileInterface(withCsr: Boolean = true) extends Module {
   val io = IO(new RegisterFileInterfaceIO)
 
+  // Memory Map:
+  // mepc  100010
+  // mtval 100011
+  // csr   1000xx
+  // rd    0xxxxx
+  val CsrPrefix = "b1000".U
+  val MepcAddress = CsrPrefix ## Csr.Mepc
+  val MtvalAddress = CsrPrefix ## Csr.Mtval
+  val MtvecAddress = CsrPrefix ## Csr.Mtvec
+
 
   if(withCsr) {
-    // write side
-    val rd = io.write.controlData |
-      (io.write.aluData & io.write.aluEnable) |
-      (io.write.csrData & io.write.csrEnable) |
-      (io.write.memData)
-    
+    ///// write side
+    val rd = io.rd.controlData |
+      (io.rd.aluData & io.rd.aluEnable) | // result from ALU
+      (io.rd.csrData & io.rd.csrEnable) | // result from CSR
+      (io.rd.memData) // result from memory
 
+    // port 0: rd, mtval
+    val mtval = Mux(io.trap.memMisaligned, io.trap.bufRegData, io.trap.badPC)
+    io.rf.write0.data := Mux(io.trap.doTrap, mtval, rd)
+    io.rf.write0.addr := Mux(io.trap.doTrap, MtvalAddress, io.rd.address)
+    io.rf.write0.enable := io.trap.doTrap || io.rd.writeEnable
 
+    // port 1: csr, mepc
+    io.rf.write1.data := Mux(io.trap.doTrap, io.trap.mePC, io.csr.writeData)
+    io.rf.write1.addr := Mux(io.trap.doTrap, MepcAddress, io.csr.address)
+    io.rf.write1.enable := io.trap.doTrap || io.csr.enable
 
+    ///// read side
+
+    // port 0: rs1
+    io.rf.read0.addr := io.rs1.address
+    io.rs1.data := io.rf.read0.data
+
+    // port 1: rs2 / csr
+    io.rf.read1.addr := MuxCase(io.rs2.address, Seq(
+      io.trap.doTrap -> MtvecAddress,
+      io.trap.mRet -> MepcAddress,
+      io.csr.enable -> CsrPrefix ## io.csr.address
+    ))
+    io.rs2.data := io.rf.read1.data
+    io.csr.readData := io.rf.read1.data & io.csr.enable
+    io.trap.csrPC := io.rf.read1.data
+
+  } else { // without CSR
+    ///// write side
+    val rd = io.rd.controlData |
+      (io.rd.aluData & io.rd.aluEnable) | // result from ALU
+      (io.rd.memData) // result from memory
+
+    // port 0: rd
+    io.rf.write0.data := rd
+    io.rf.write0.addr := io.rd.address
+    io.rf.write0.enable := io.rd.writeEnable
+
+    // port 1: unused
+    io.rf.write1.data := 0.U
+    io.rf.write1.addr := 0.U
+    io.rf.write1.enable := false.B
+
+    ///// read side
+
+    // port 0: rs1
+    io.rf.read0.addr := io.rs1.address
+    io.rs1.data := io.rf.read0.data
+
+    // port 1: rs2
+    io.rf.read1.addr := io.rs2.address
+    io.rs2.data := io.rf.read1.data
+
+    io.csr.readData := DontCare
+    io.trap.csrPC := DontCare
   }
-
 }
 
 class RegisterFileInterfaceIO extends Bundle {
-  val rf = Flipped(new RegisterFileIO)
+  val rf = Flipped(new RegisterFilePortIO)
   val trap = new RegisterFileTrapIO
   val csr = new RegisterFileToCsrIO
-  val write = new RegisterFileWritePortIO
+  val rd = new RegisterFileWritePortIO
   val rs1 = new RegisterFileReadPortIO
   val rs2 = new RegisterFileReadPortIO
 }
@@ -48,7 +110,7 @@ class RegisterFileWritePortIO extends Bundle {
 }
 
 class RegisterFileTrapIO extends Bundle {
-  val trap = Input(Bool())
+  val doTrap = Input(Bool())
   val mRet = Input(Bool())
   val mePC = Input(Bool())
   val memMisaligned = Input(Bool())
---
+++ test/serv/RegisterFileInterfaceSpec.scala
@@ -0,0 +1,23 @@
+// Copyright 2020 The Regents of the University of California
+// released under BSD 2-Clause License
+// author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+
+package serv
+
+import org.scalatest._
+import chisel3.tester._
+import chiseltest.internal.WriteVcdAnnotation
+import chisel3._
+
+class RegisterFileInterfaceSpec extends FlatSpec with ChiselScalatestTester  {
+  val WithVcd = Seq(WriteVcdAnnotation)
+  val NoVcd = Seq()
+
+  it should "elaborate w/ csr" in {
+    test(new RegisterFileInterface(true)) { dut => }
+  }
+
+  it should "elaborate w/o csr" in {
+    test(new RegisterFileInterface(false)) { dut => }
+  }
+}
