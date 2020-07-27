b85a948a5c53161211dc9b615a07393c8690ad1e
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-07-26 18:09:52 -0700

    top: try to execute a load instruction

--- test/serv/ServTopWithRamSpec.scala
+++ test/serv/ServTopWithRamSpec.scala
@@ -22,14 +22,25 @@ class ServTopWithRamSpec extends FlatSpec with ChiselScalatestTester  {
     test(new ServTopWithRam(false)) { dut => }
   }
 
-  def add(rs1: Int, rs2: Int, rd: Int): UInt = {
-    val funct7 = "0000000"
-    val funct3 = "000"
-    val opcode = "0110011"
-    val instruction = funct7 + bitString(rs2, 5) + bitString(rs1, 5) + funct3 + bitString(rd, 5) + opcode
-    assert(instruction.length == 32)
-    ("b" + instruction).U
+  it should "add" in {
+    val random = new scala.util.Random(0)
+    val model = new RiscvModel(random, true)
+    test(new ServTopWithRam(true)).withAnnotations(WithVcd)  { dut =>
+      val rs1 = random.nextInt(32)
+      val rs2 = random.nextInt(32)
+      val rd = random.nextInt(32)
+      model.add(rd, rs1, rs2)
+      exec(dut.clock, dut.io, RiscV.add(rs1, rs2, rd))
+    }
   }
+
+  it should "load" in {
+    test(new ServTopWithRam(true)).withAnnotations(WithVcd)  { dut =>
+      val i = RiscV.loadWord(0xab, 0, 1) // load address 0xab into x1
+      exec(dut.clock, dut.io, i)
+    }
+  }
+
 
   def exec(clock: Clock, dut: ServTopWithRamIO, instruction: UInt): Unit = {
     // disable interrupts
@@ -59,21 +70,69 @@ class ServTopWithRamSpec extends FlatSpec with ChiselScalatestTester  {
     }
   }
 
-  it should "add" in {
-    val random = new scala.util.Random(0)
-    val model = new RiscvModel(random, true)
-    test(new ServTopWithRam(true)).withAnnotations(WithVcd)  { dut =>
-      val rs1 = random.nextInt(32)
-      val rs2 = random.nextInt(32)
-      val rd = random.nextInt(32)
-      model.add(rd, rs1, rs2)
-      exec(dut.clock, dut.io, add(rs1, rs2, rd))
-    }
+
+}
+
+object RiscV {
+  /** rd = *(rs1 + offset) */
+  def loadWord(offset: Int, rs1: Int, rd: Int): UInt = // LW
+    i(imm = offset, rs1=rs1, funct3 = "010", rd=rd, opcode = "0000011")
+  /**  */
+  def storeWord(offset: Int, rs1: Int, rs2: Int): UInt = // SW
+    s(imm = offset, rs2=rs2, rs1=rs1, funct3 = "010", opcode = "0100011")
+
+  def add(rs1: Int, rs2: Int, rd: Int): UInt =
+    r(funct7 = "0000000", rs2 = rs2, rs1 = rs1, funct3 = "000", rd = rd, opcode = "0110011")
+
+  val MaxImm12: BigInt = (BigInt(1) << 11) - 1
+  val MinImm12: BigInt = -(MaxImm12 + 1)
+
+  def i(imm: BigInt, rs1: Int, funct3: String, rd: Int, opcode: String): UInt = {
+    assert(imm <= MaxImm12 && imm >= MinImm12)
+    assert(rs1 >= 0 && rs1 < 32)
+    assert(funct3.length == 3)
+    assert(rd >= 0 && rd < 32)
+    assert(opcode.length == 7)
+    val instr = bitString(imm, 12) + unsigned(rs1, 5) + funct3 + unsigned(rd, 5) + opcode
+    assert(instr.length == 32)
+    ("b" + instr).U
   }
 
+  def s(imm: BigInt, rs2: Int, rs1: Int, funct3: String, opcode: String): UInt = {
+    assert(imm <= MaxImm12 && imm >= MinImm12)
+    assert(rs2 >= 0 && rs2 < 32)
+    assert(rs1 >= 0 && rs1 < 32)
+    assert(funct3.length == 3)
+    assert(opcode.length == 7)
+    val mask12 = (BigInt(1) << 12) - 1
+    val mask5 = (BigInt(1) << 5) - 1
+    val immMsb = (imm & mask12) >> 5
+    val immLsb = imm & mask5
+    val instr = bitString(immMsb, 12 - 5) + unsigned(rs2, 5)  + unsigned(rs1, 5) +
+      funct3 + bitString(immLsb, 5) + opcode
+    assert(instr.length == 32)
+    ("b" + instr).U
+  }
 
-  private def bitString(i: Int, width: Int): String = {
+  def r(funct7: String, rs2: Int, rs1: Int, funct3: String, rd: Int, opcode: String): UInt = {
+    assert(funct7.length == 7)
+    assert(rs2 >= 0 && rs2 < 32)
+    assert(rs1 >= 0 && rs1 < 32)
+    assert(funct3.length == 3)
+    assert(rd >= 0 && rd < 32)
+    assert(opcode.length == 7)
+    val instr = funct7 + unsigned(rs2, 5) + unsigned(rs1, 5) + funct3 + unsigned(rd, 5) + opcode
+    assert(instr.length == 32)
+    ("b" + instr).U
+  }
+  private def unsigned(i: Int, width: Int): String = {
     val str = Integer.toBinaryString(i)
+    assert(str.length <= width)
+    if(str.length == width) { str } else { ("0" * (width - str.length)) + str }
+  }
+  private def bitString(i: BigInt, width: Int): String = {
+    val mask = (BigInt(1) << width) - 1
+    val str = (i & mask).toString(2)
     assert(str.length <= width)
     if(str.length == width) { str } else { ("0" * (width - str.length)) + str }
   }
