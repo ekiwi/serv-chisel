29365e65c2e884cb050c266979fe5b7a953d9344
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-12-02 14:40:29 -0800

    change wishbone ram to use four chisel memories

--- src/servant/WishBoneRam.scala
+++ src/servant/WishBoneRam.scala
@@ -9,26 +9,22 @@ package servant
 import chisel3._
 import chisel3.util._
 
-class WishBoneRam(depth: Int = 256) extends Module {
+class WishBoneRam(depth: Int = 256, preload: List[BigInt] = List()) extends Module {
   val addressWidth = log2Ceil(depth)
   val io = IO(wishbone.WishBoneIO.Responder(addressWidth))
 
-  val mem = SyncReadMem(depth / 4, Vec(4, UInt(8.W)))
+  val mem = Seq.fill(4)(SyncReadMem(depth / 4, UInt(8.W)))
   val wordAlignedAddress = io.adr(addressWidth - 1, 2)
 
   val ack = Reg(Bool())
   ack := io.cyc && !ack
   io.ack := ack
 
-  io.rdt := mem.read(wordAlignedAddress).asUInt()
+  io.rdt := Cat(mem.map(_.read(wordAlignedAddress, true.B)))
 
-  //val writeData = Seq(io.dat(31,24), io.dat(23,16), io.dat(15,8), io.dat(7,0))
-  // asUInt on vectors returns a weird order
-  val writeData = Seq(io.dat(7,0), io.dat(15,8), io.dat(23,16), io.dat(31,24))
-  val writeMask = io.sel.asBools()
-  when(io.cyc && io.we) {
-    mem.write(wordAlignedAddress, VecInit(writeData), VecInit(writeMask))
+  val writeData = Seq(io.dat(31,24), io.dat(23,16), io.dat(15,8), io.dat(7,0))
+  val writeMask = io.sel.asBools().reverse
+  mem.zip(writeMask).zip(writeData).foreach { case ((m, mask), data) =>
+    when(mask && io.cyc && io.we) { m.write(wordAlignedAddress, data) }
   }
 }
-
-
