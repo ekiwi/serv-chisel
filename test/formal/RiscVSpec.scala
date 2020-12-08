1f3a8fb8c47a446591457ce398777725cacea9f1
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-12-08 12:01:54 -0800

    add RsicVSpec

--- test/formal/RiscVSpec.scala
+++ test/formal/RiscVSpec.scala
@@ -1,7 +1,57 @@
+// Copyright 2020 The Regents of the University of California
+// released under BSD 2-Clause License
+// author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+
 package formal
 
+import org.scalatest._
 import paso._
+import chisel3._
 
-class RiscVSpec {
+class RiscVSpec extends UntimedModule {
+  val reg = Mem(31, UInt(32.W))
+
+  private def readReg(addr: UInt): UInt = {
+    require(addr.getWidth == 5)
+    Mux(addr === 0.U, 0.U, reg.read(addr - 1.U))
+  }
+
+  private def updateReg(addr: UInt, data: UInt): Unit = {
+    require(addr.getWidth == 5)
+    require(data.getWidth == 32)
+    when(addr =/= 0.U) {
+      reg.write(addr - 1.U, data)
+    }
+  }
+
+  private def rType(in: RTypeIO, op: (UInt, UInt) => UInt): Unit =
+    updateReg(in.rd, op(readReg(in.rs1), readReg(in.rs2)))
+  private def iType(in: ITypeIO, op: (UInt, UInt) => UInt): Unit =
+    updateReg(in.rd, op(readReg(in.rs1), in.decodeImm))
+
+  val add = fun("add").in(new RTypeIO)(rType(_, (a,b) => a + b))
+  val addi = fun("addi").in(new ITypeIO)(iType(_, (a,b) => a + b))
+}
+
+class RTypeIO extends Bundle {
+  val rs1 = UInt(5.W)
+  val rs2 = UInt(5.W)
+  val rd = UInt(5.W)
+}
+
+class ITypeIO extends Bundle {
+  val imm = SInt(12.W)
+  val rs1 = UInt(5.W)
+  val rd = UInt(5.W)
+  def decodeImm: UInt = imm.pad(32).asUInt()
+}
+
+class CompileRiscVSpec extends FlatSpec {
+  behavior of "RiscVSpec"
+
+  // manye Chisel/Paso errors are only caught when elaborating
+  it should "correctly elaborate" in {
+    val spec = UntimedModule(new RiscVSpec)
+  }
 
 }
